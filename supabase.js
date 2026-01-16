f6ae39131959acc1b6dda1d0151b1d18eb7aa2b7
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-16 10:55:56 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,9 +1,9 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const SUPABASE_URL = ''
-const SUPABASE_ANON_KEY = ''
+const SUPABASE_URL = 'https://qdgsmnfhpbkbovptwwjp.supabase.co'
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA'
 
 export const supabase = createClient(
-  const SUPABASE_URL = 'https://qdgsmnfhpbkbovptwwjp.supabase.co',
-  const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2ODE1NDUzNiwiZXhwIjoyMDgzNzMwNTM2fQ.W5iFRYrGVhgc9ySXdBgl00A1K1lHyjAz5ZTIP7C4ZrA)'
+  https://qdgsmnfhpbkbovptwwjp.supabase.co,
+  eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkZ3NtbmZocGJrYm92cHR3d2pwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNTQ1MzYsImV4cCI6MjA4MzczMDUzNn0.-fpxGBv738LflicnI2edM7ywwAmed3Uka4111fzTj8c
 )
