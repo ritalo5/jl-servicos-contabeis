535403c5b1ae2d79f8d7274ffe0325b56850b863
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-19 16:56:05 -0300

    Update certidoes.js

--- servicos/certidoes-regularizacoes/certidoes.js
+++ servicos/certidoes-regularizacoes/certidoes.js
@@ -2,17 +2,17 @@ const servicosCertidoes = {
   'certidao-negativa': {
     titulo: 'Certidão Negativa de Débitos',
     descricao: 'Emissão de certidões federais, estaduais ou municipais.',
-    valor: 'R$ 90,00'
+    valor: 'R$ 79,99'
   },
   'regularizacao-cnpj': {
     titulo: 'Regularização de CNPJ',
     descricao: 'Correção de pendências cadastrais.',
-    valor: 'R$ 180,00'
+    valor: 'R$ 149,99'
   },
   'regularizacao-fiscal': {
     titulo: 'Regularização Fiscal Completa',
     descricao: 'Análise e regularização de débitos fiscais.',
-    valor: 'R$ 300,00'
+    valor: 'R$ 149,99'
   }
 }
 
