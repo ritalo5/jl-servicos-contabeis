20c4ea624219efe23c6e15e64be0bb11e58ee070
joaovss1606-bit <joaovitorss1606@gmail.com>
2026-01-19 16:49:39 -0300

    Update certificado.js

--- servicos/certificado-digital/certificado.js
+++ servicos/certificado-digital/certificado.js
@@ -2,17 +2,17 @@ const servicosCertificadoDigital = {
   'certificado-a1': {
     titulo: 'Certificado Digital A1',
     descricao: 'Certificado digital em arquivo eletrônico.',
-    valor: 'R$ 180,00'
+    valor: 'R$ 189,99'
   },
   'certificado-a3': {
     titulo: 'Certificado Digital A3',
     descricao: 'Certificado digital em token ou cartão.',
-    valor: 'R$ 220,00'
+    valor: 'R$ 249,99'
   },
   'renovacao-certificado': {
     titulo: 'Renovação de Certificado Digital',
     descricao: 'Renovação de certificados vencidos.',
-    valor: 'R$ 150,00'
+    valor: 'R$ 149,99'
   }
 }
 
